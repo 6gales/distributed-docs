19a2522b2c8127815d8b37cb5d22d5a6d39fdd53
Andrey <6gales@gmail.com>
2021-05-10 15:15:10 +0700

    Fix guid id navigation

--- DistributedDocs/DistributedDocs.Client/DocumentManager.cs
+++ DistributedDocs/DistributedDocs.Client/DocumentManager.cs
@@ -1,34 +1,33 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using DistributedDocs.Server.ClientModels;
 
 namespace DistributedDocs.Client
 {
-    public static class DocumentManager
+    internal static class DocumentManager
     {
         private static readonly Dictionary<Guid, DocumentInfo> Documents = new Dictionary<Guid, DocumentInfo>();
 
         public static IReadOnlyCollection<DocumentInfo> DocumentsInfos => Documents.Values;
-        
-        public static void AddDocument(DocumentInfo document) 
+
+        public static void AddDocument(DocumentInfo document)
         {
             Documents.Add(document.DocumentId, document);
         }
-        public static DocumentInfo? GetDocument(Guid documentId) 
+
+        public static DocumentInfo? GetDocument(Guid documentId)
         {
             if (Documents.TryGetValue(documentId, out var info))
             {
                 return info;
             }
+
             return null;
         }
+
         public static string Url(Guid documentId)
-        { 
-            return "documents/" + Documents
-                .Where(kv => kv.Key == documentId)
-                .Select((kv, i) => i)
-                .FirstOrDefault();
+        {
+            return $"documents/{documentId}";
         }
     }
 }
